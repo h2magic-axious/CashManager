aa544ae5c4315bb9357369ffe20dcc9293cc5092
soleperson <h2magic@126.com>
2021-08-19 16:29:31 +0800

    init mysql information

--- CashManager/settings.py
+++ CashManager/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'CashManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cash_manager',
+        'USER': 'cash_manager',
+        'PASSWORD': 'NFAcFyTCkyAdmL5f',
+        'HOST': '149.129.65.210',
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -106,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
