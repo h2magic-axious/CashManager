7ea4cc29440198c76aa68967eeef572504a09789
soleperson <h2magic@126.com>
2021-08-19 16:34:35 +0800

    reback to sqlite3

--- CashManager/settings.py
+++ CashManager/settings.py
@@ -78,15 +78,8 @@ WSGI_APPLICATION = 'CashManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cash_manager',
-        'USER': 'cash_manager',
-        'PASSWORD': 'NFAcFyTCkyAdmL5f',
-        'HOST': '149.129.65.210',
-        'PORT': 3306,
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR.joinpath('db.sqlite3')
     }
 }
 
